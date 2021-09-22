d85af692a671a053be7a0ddd91924c88df8d439a
Deniz Dalkilic <deniz@bounty0x.io>
2021-09-22 21:06:36 +0100

    Correnction to contract name

--- contracts/MRHBToken.sol
+++ contracts/MRHBToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PBXToken is Ownable, ERC20Burnable {
+contract MRHBToken is Ownable, ERC20Burnable {
   string private constant _name = "Marhaba DeFi";
   string private constant _symbol = "MRHB";
 
